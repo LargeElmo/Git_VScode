df5c0b948d50158300fe8b2c966628ee85555ea7
LargeElmo <jimferguson@att.net>
2022-06-14 16:43:40 -0400

    added custom 'wherifications.wherify()

---
+++ wherifications.py
@@ -0,0 +1,16 @@
+# def main():
+    # wherify()
+    
+def wherify():
+    import os, sys
+    print("\nPython interpreter: ", sys.executable)
+    print("Working directory:     ",os.getcwd())
+    print("Python version:          ", sys.version, "\n")
+    return
+
+					
+
+# if __name__ == "__main":
+    main()
+
+# main()
