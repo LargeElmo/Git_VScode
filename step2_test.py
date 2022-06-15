af279438a0f005326ba63ead01556d16f0930250
LargeElmo <jimferguson@att.net>
2022-06-15 11:53:15 -0400

    examples of more complex list methods

--- step2_test.py
+++ step2_test.py
@@ -3,3 +3,19 @@
 
 a = ["apples","amazon","actual","armoured"] 
 print(a)
+a.append("boomerang")
+print(a)
+b = ["banana", "bozo", "beans"]
+a.append(b)
+print(a)
+print(type(a[5]))
+c = ("False", 0.75, "slavartnick", -99, [67, 0, 1001])
+print(type(c))
+a.append(c)
+print(a)
+print(type(a[-1]))
+print(a[-1])
+print(type(a[-1:-2]))
+print(a[6][3])
+
+
